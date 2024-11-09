ba24898aabdbb0c62636a9d84195130d3ba5a4f5
jabarrs <jabaruddinsyukri@gmail.com>
2024-11-10 02:48:58 +0700

    update

---
+++ src/axios.js
@@ -0,0 +1,15 @@
+// import axios from "axios";
+
+// Mengatur axios di window untuk akses global
+// window.axios = axios;
+
+// Menetapkan konfigurasi default untuk axios
+// axios.defaults.withCredentials = false;
+
+// Menggunakan HTTPS dan menggabungkan hostname dengan URL backend
+// let backendUrl = `https://be-rest.vercel.app`;
+
+// Atur baseURL axios
+// axios.defaults.baseURL = backendUrl;
+
+// console.log('Base URL:', axios.defaults.baseURL);
